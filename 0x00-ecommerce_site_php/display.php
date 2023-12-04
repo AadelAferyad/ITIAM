46150da76801b0a8fc94b584bba262f435e360b2
AadelAferyad <animevineu@gmail.com>
2023-12-04 15:25:09 +0100

    water fiji

--- 0x00-ecommerce_site_php/display.php
+++ 0x00-ecommerce_site_php/display.php
@@ -28,6 +28,14 @@
         echo "Erorr".$stmt->error;
     $stmt->close();
     $conn->close();
+    if (isset($_POST['update']))
+    {
+        
+        $conn = new mysqli ("localhost", "haruma", "kjkszpj987", "Test1");
+        if ($conn->connect_error)
+            die("Failed".$conn->connect_error);
+        $stmt = $conn->prepare("UPDATE sharshar SET title LIKE ?");
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -37,8 +45,17 @@
     <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
-<body>
-    <input type="submit" value="Update" style="width:200px;">
-    
+<body style="height:100%;position:realative;">
+    <form action="display.php" style="position:absolute;left:35%; padding-bottom:5%;" method="post">
+            <label>Id:</label><input type="text"  name="id" >
+            <label>Title:<input type="text"  name="title" ></label>
+            <label>Old Price:<input type="text"  name="price_1" ></label>
+            <label>New Price:<input type="text"  name="price_2" ></label>
+            <input type="file"  name="img">
+        <input type="submit" value="Update" style="width:200px; " name="update">
+        <input type="submit" value="Delete" style="width:200px; ">    
+
+    </form>
 </body>
 </html>
+
